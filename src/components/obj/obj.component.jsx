de7f7a63debc0a5e28663235e614165a5bd330af
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-05-01 23:21:42 +0200

    corrected obj page

--- src/components/obj/obj.component.jsx
+++ src/components/obj/obj.component.jsx
@@ -16,29 +16,27 @@ const Obj = () => {
         <Col xs={12} md={6} lg={2} xxl={2} className="home-container" style={{ paddingLeft: 0 }}>
           <Home />
         </Col>
-        <Col xs={12} md={6} lg={8} xxl={10} className="obj-container">
-<div className="obj-div1">
-<img src={objImg1} alt="obj-img-1" className="obj-img1" />
+        <Col xs={12} md={6} lg={8} xxl={8} className="obj-container">
 
+         <div className="obj-div1">
+         <img src={objImg1} alt="obj-img-1" className="obj-img1" />
+         <img src={objText} alt="obj-text" className="obj-text" />
+         <div className="obj-img2-div">
+         <h2 className="obj-img2-header">Roots.obj</h2>
+        <img src={objImg2} alt="obj-img-1" className="obj-img2" />
+         </div>
+        
+         </div>
+         <div className="obj-div2">
+<div className="obj-img3-div">
 <h2 className="obj-img3-header">Flask.obj</h2>
-      <img src={objImg3} alt="obj-img-3" className="obj-img3" />
+      <img src={objImg3} alt="obg-img-3" className="obj-img3" />
       <p className="obj-text3">Deciding whether to bring a bag for my bottle or buy one and contribute to environmental pollution with unnecessary plastic was an everyday dilemma. Drawing inspiration from a military-style flask, an ergonomic bottle that attaches to the side of the bottle came to existence.</p>
       <p className="obj-text3-part2">The holder for a metal bottle is constructed from sturdy resin and features a metal plate that securely clasps the bottle. By gently squeezing the holder from both sides, the bottle can be easily removed and reattached with equal simplicity.</p>
-
-
 </div>
-<div className="obj-div2">
-<img src={objText} alt="obj-text" className="obj-text" />
-<img src={objImg4} alt="obj-img4" className="obj-img4" />
-</div>
-<div className="obj-div3">
-<h2 className="obj-img2-header">Roots.obj</h2>
-        <img src={objImg2} alt="obg-img-1" className="obj-img2" />
-        <img src={objImg5} alt="obj-img4" className="obj-img5" />
-</div>
-
-
-
+<img src={objImg4} alt="obg-img-1" className="obj-img4" />
+<img src={objImg5} alt="obg-img-5" className="obj-img5" />
+         </div>
         </Col>
       </Row>
       
